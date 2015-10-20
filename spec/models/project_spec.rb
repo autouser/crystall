9db4054c16d9b5ed7aa1b9558f3c68cc31a72814
Evgeny Grep <gyorms@gmail.com>
2015-10-20 11:51:30 +0300

    Switch Project unit test to use Project factory

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,43 +2,47 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
 
-  # let(:user) { User.create! username: 'john', password: 'test1234' }
   let(:user) { create :user }
 
-  describe "when instantiated", focus: true do
+  describe "when instantiated" do
     
     context "with correct arguments" do
 
-      let(:project) { Project.new(user: user, name: 'Core 1.0', description: 'Core System', status: 'open') }
+      let(:project) { build :project, user: user }
 
       it { expect( project ).to be_valid }
 
     end
 
     context "with empty name" do
-      let(:project) { Project.new(user: user, description: 'Core System', status: 'open') }
+      let(:project) { build :project, user: user, name: nil }
+
       it { expect( project ).to have_one_error(:name, "can't be blank") }
     end
 
     context "with non unique name" do
-      before(:example) { Project.create!(user: user, name: 'Core 1.0', description: 'Core System', status: 'open') }
-      let(:project) { Project.new(user: user, name: 'Core 1.0', description: 'Core System', status: 'open') }
+      before(:example) { create :project, user: user }
+      let(:project) { build :project, user: user }
+
       it { expect( project ).to have_one_error(:name, "has already been taken") }
     end
 
     context "with empty status" do
-      let(:project) { Project.new(user: user, name: 'Core 1.0', description: 'Core System', status: nil) }
+      let(:project) { build :project, user: user, status: nil }
+
       it { expect( project ).to have_one_error(:status, "is not included in the list") }
     end
 
     context "with wrong status" do
-      let(:project) { Project.new(user: user, name: 'Core 1.0', description: 'Core System', status: "wrong") }
+      let(:project) { build :project, user: user, status: 'wrong' }
+
       it { expect( project ).to have_one_error(:status, "is not included in the list") }
     end
 
 
     context "with empty user" do
-      let(:project) { Project.new(user: nil, name: 'Core 1.0', description: 'Core System', status: "open") }
+      let(:project) { build :project, user: nil }
+      
       it { expect( project ).to have_one_error(:user, "can't be blank") }
     end
 
